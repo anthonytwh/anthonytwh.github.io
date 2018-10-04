6bdb024144d98951906d46459c0654bdbbe50a06
Anthony Tam <anthonyy.tam@gmail.com>
2018-10-04 16:20:56 -0400

    Update Footer format & urls

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,31 +16,42 @@ class Footer extends Component {
 	render() {
 		return (
 			<div className="Footer-div">
-				<Card>
-					<CardBody className="Footer-body">
-						<Row> 
-							<Col>
-								<CardTitle>Connect With Me!</CardTitle>
-								<CardText><p> See my social media on the right</p></CardText>
-							</Col>
-							<Col>
-								<CardTitle></CardTitle>
-							</Col>
-							<Col>
-								<CardTitle className="Footer-blank">Peeka-Boo!</CardTitle>
-								<CardText>
-									<Row>
-										<Col xs={{ size: 'auto', offset: 4 }}><FontAwesomeIcon icon={faGithub} size="lg" /></Col>
-										<Col xs={{ size: 'auto', offset: 0 }}><FontAwesomeIcon icon={faLinkedin} size="lg" /></Col>
-										<Col xs={{ size: 'auto', offset: 0 }}><FontAwesomeIcon icon={faInstagram} size="lg" /></Col>
-										<Col xs={{ size: 'auto', offset: 0 }}><FontAwesomeIcon icon={faMedium} size="lg" /></Col>
-									</Row>
-								</CardText>
-							</Col>
-						</Row>
-					</CardBody>
-					<CardFooter className="Footer-foot"> (C) anthonyt.ca </CardFooter>
-				</Card>
+			<br></br>
+			<br></br>
+				<Row> 
+					<Col>
+						<h1 className="Footer-head"> Connect with me! </h1>
+						<br></br>
+						<p className="Footer-body"> hello[at]anthonyt.ca </p>
+					</Col>
+					<Col>
+						<br></br>
+						<br></br>
+					</Col>
+					<Col>
+						<br></br>
+						<br></br>
+						<p>
+							<Row>
+								<Col xs={{ size: 'auto', offset: 4 }}>
+									<a className="Footer-icons" href="https://github.com/anthonytwh">
+										<FontAwesomeIcon icon={faGithub} size="lg" /></a></Col>
+								<Col xs={{ size: 'auto', offset: 0 }}>
+									<a className="Footer-icons" href="https://www.linkedin.com/in/anthony-tam/">
+										<FontAwesomeIcon icon={faLinkedin} size="lg" /></a></Col>
+								<Col xs={{ size: 'auto', offset: 0 }}>
+									<a className="Footer-icons" href="https://www.instagram.com/anthonyytam/">
+										<FontAwesomeIcon icon={faInstagram} size="lg" /></a></Col>
+								<Col xs={{ size: 'auto', offset: 0 }}>
+									<a className="Footer-icons" href="https://medium.com/@AnthonyTam">
+										<FontAwesomeIcon icon={faMedium} size="lg" /></a></Col>
+							</Row>
+						</p>
+					</Col>
+				</Row>
+				<br></br>
+				<br></br>
+			<div className="Footer-foot"> (C) anthonyt.ca </div>
 			</div>
 			)
 	}
