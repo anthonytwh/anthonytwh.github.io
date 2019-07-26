9863085f8b2bd967b0200f10755ca943218e2be0
Anthony Tam <anthonyy.tam@gmail.com>
2019-07-26 01:48:51 -0400

    Updates

---
+++ precache-manifest.c2581d6c5c75b7b6fbeb4731c8b1d3d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bba7f408ebd27756c5ecf52dfc829bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61fe8fb4dfa28f4c90f9",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "62485e6817817ca01c18",
+    "url": "/static/css/main.a12017f3.chunk.css"
+  },
+  {
+    "revision": "61fe8fb4dfa28f4c90f9",
+    "url": "/static/js/2.b7a97492.chunk.js"
+  },
+  {
+    "revision": "62485e6817817ca01c18",
+    "url": "/static/js/main.225e5043.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "91db3e92718cec28be3fcf3e79383201",
+    "url": "/static/media/Anthony_Resume.91db3e92.pdf"
+  },
+  {
+    "revision": "d808fef0da889f09742d3a074f356bc5",
+    "url": "/static/media/background.d808fef0.png"
+  },
+  {
+    "revision": "6633a16d942ac9d7e1a4ad258417fc8d",
+    "url": "/static/media/background2.6633a16d.png"
+  },
+  {
+    "revision": "9a30e6c6c75344e5bd217bbe9720ef3e",
+    "url": "/static/media/mid-section.9a30e6c6.png"
+  },
+  {
+    "revision": "a64925422d21728473d89c3493cdc26b",
+    "url": "/static/media/profilephoto.a6492542.jpg"
+  }
+]);
