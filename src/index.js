f2a892e255e92ba133df3af52c37d1b2fd709076
Anthony Tam <anthonyy.tam@gmail.com>
2019-07-26 23:30:58 -0400

    remove index.css dependencies

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import './assets/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App.jsx';
