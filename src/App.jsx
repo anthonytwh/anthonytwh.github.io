d044311e7659443a01bc1bbfc83ac0f3ef07e37b
Anthony Tam <anthonyy.tam@gmail.com>
2021-02-07 14:51:41 -0500

    fix nav-link, and true mobile/tablet versions

--- src/App.jsx
+++ src/App.jsx
@@ -16,33 +16,74 @@ class App extends Component {
     constructor (props){
         super(props);
         this.state = {
-            isMobile: false
+            isMobile: false,
+            isTablet: false
         };
     }
     
     componentDidMount() {
         this.props.media({ minWidth: 768 }, () => {
             this.setState({
-                isMobile: false
+                isTablet: false
             });
         });
 
         this.props.media({ maxWidth: 768 }, () => {
+            this.setState({ 
+                isTablet: true
+            });
+        });
+
+        this.props.media({ minWidth: 320 }, () => {
+            this.setState({
+                isMobile: false
+            });
+        });
+
+        this.props.media({ maxWidth: 767 }, () => {
             this.setState({ 
                 isMobile: true
             });
         });
     }
 
+    // componentDidUpdate() {
+    //     this.props.media({ minWidth: 768 }, () => {
+    //         this.setState({
+    //             isTablet: false
+    //         });
+    //     });
+
+    //     this.props.media({ maxWidth: 768 }, () => {
+    //         this.setState({ 
+    //             isTablet: true
+    //         });
+    //     });
+
+    //     this.props.media({ minWidth: 320 }, () => {
+    //         this.setState({
+    //             isMobile: false
+    //         });
+    //     });
+
+    //     this.props.media({ maxWidth: 767 }, () => {
+    //         this.setState({ 
+    //             isMobile: true
+    //         });
+    //     });
+    // }
+
     render() {
         const { isMobile } = this.state;
+        const { isTablet } = this.state;
 
         return (
             <Router>
                 {isMobile ? 
                     // Mobile //
-                    <div className="Global">
+                    <div className="Global-mobile">
                         <div className="BG-1">
+                            MOBILE
                             <div className="Main"><Main/></div>
                             <div className="About"><About/></div>
                         </div>
@@ -50,12 +91,12 @@ class App extends Component {
                         <div className="BG-2">
                             <div className="Portfolio"><Portfolio/></div>
                         </div>
-                        <div className="Footer"><Footer /></div>
-                    </div>: 
-                        // Not Mobile //
+                        <div className="Footer-mobile"><Footer /></div>
+                    </div>: isTablet ?
+                        // Tablet //
                         <div className="Global">
                             <div className="BG-1">
-                                <div className="Header"><Header/></div>
+                                TABLET
                                 <div className="Main"><Main/></div>
                                 <div className="About"><About/></div>
                             </div>
@@ -64,7 +105,21 @@ class App extends Component {
                                 <div className="Portfolio"><Portfolio/></div>
                             </div>
                             <div className="Footer"><Footer /></div>
-                        </div>
+                        </div>:
+                            // Not Mobile or Tablet //
+                            <div className="Global">
+                                <div className="BG-1">
+                                    DESKTOP
+                                    <div className="Header"><Header/></div>
+                                    <div className="Main"><Main/></div>
+                                    <div className="About"><About/></div>
+                                </div>
+
+                                <div className="BG-2">
+                                    <div className="Portfolio"><Portfolio/></div>
+                                </div>
+                                <div className="Footer"><Footer /></div>
+                            </div>
                 }
             </Router>
         );
