90e20a6bc25766df70f7c3531ed4fe5239fe34c3
Anthony Tam <anthonyy.tam@gmail.com>
2025-08-08 00:00:34 -0400

    update Google Tag Manager

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,16 @@ function App() {
   const handleGitHubClick = (e) => {
     e.preventDefault();
     
-    // Track the GitHub link click
+    // Track the GitHub link click with Google Tag Manager
+    if (typeof window !== 'undefined' && window.gtag) {
+      window.gtag('event', 'click', {
+        event_category: 'Outbound Link',
+        event_label: 'GitHub Profile',
+        value: 1
+      });
+    }
+    
+    // Also track with legacy Google Analytics if available
     if (typeof window !== 'undefined' && window.ga) {
       window.ga('send', 'event', {
         eventCategory: 'Outbound Link',
@@ -24,7 +33,7 @@ function App() {
     <div className="App">
       <div className="container">
         <h1>Anthony Tam</h1>
-        <p>Did you come here from LinkedIn? Here are links to some of my work and contact info.</p>
+        <p>Did you come here from LinkedIn? If so, you're in the right place.</p>
         <p>Github: <a href="https://github.com/anthonytwh" onClick={handleGitHubClick}>anthonytwh</a></p>
         <p>Contact me: <SpamFilter emailAddress="contact@anthonyt.ca" /></p>
       </div>
